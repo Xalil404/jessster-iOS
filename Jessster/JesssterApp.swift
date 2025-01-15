822f18bb47082b38d02231bc34f9019e9b1f6e4c
Xalil404 <kareemxalil@gmail.com>
2025-01-15 13:17:05 +0000

    Removed GoogleService-Info.plist to protect sensitive data

--- Jessster/JesssterApp.swift
+++ Jessster/JesssterApp.swift
@@ -4,6 +4,41 @@
 //
 //  Created by TEST on 10.01.2025.
 //
+
+import GoogleSignIn
+import SwiftUI
+import FirebaseCore
+
+// AppDelegate class for Firebase setup
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct JesssterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    // Register AppDelegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onOpenURL { url in
+                    GIDSignIn.sharedInstance.handle(url)  // Handle the URL for Google Sign-In
+                }
+        }
+    }
+}
+
+/*
 import GoogleSignIn
 import SwiftUI
 
@@ -21,4 +56,4 @@ struct JesssterApp: App {
         }
     }
 }
-
+*/
